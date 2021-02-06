78a02c7956446cbc4ea7d59bd28473d6fcb1acfc
Cole Schlesinger <cole@akitasoftware.com>
2021-02-05 17:56:54 -0800

    Use akita_django.Client for testing
    The Akita Django Client extends django.test.Client, intercepting
    requests and responses and logging them to an HTTP Archive (HAR)
    file.

--- akiboxImpl/tests.py
+++ akiboxImpl/tests.py
@@ -1,17 +1,18 @@
 import copy
-import json
+
+from akita_django.test import Client
+from django.test import TestCase
 from rest_framework import status
-from django.test import TestCase, Client
-from django.urls import reverse
+
 from .models import User, File
 from .serializers import UserSerializer, FileSerializer
-
-# Initialize the APIClient app
-client = Client()
 
 
 class UserTest(TestCase):
     """Tests for user-related APIs."""
+
+    def setUp(self):
+        self.client = Client(har_file_path='akita_trace.user.har')
 
     def test_user_crud(self):
         kent = {
@@ -22,7 +23,7 @@ class UserTest(TestCase):
         }
 
         # Create user
-        response = client.post('/users/', kent)
+        response = self.client.post('/users/', kent, content_type="application/json")
         expected = copy.deepcopy(kent)
         expected["id"] = response.data["id"]
 
@@ -30,27 +31,33 @@ class UserTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Get user
-        response = client.get(f'/users/{expected["id"]}/')
+        response = self.client.get(f'/users/{expected["id"]}/')
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Update user
         expected["phone"] = "415-999-8877"
-        response = client.put(f'/users/{expected["id"]}/', data=expected, content_type='application/json')
+        response = self.client.put(f'/users/{expected["id"]}/', data=expected, content_type='application/json')
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Delete user
-        response = client.delete(f'/users/{expected["id"]}/')
+        response = self.client.delete(f'/users/{expected["id"]}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         # Try to get user, should fail
-        response = client.get(f'/users/{expected["id"]}/')
+        response = self.client.get(f'/users/{expected["id"]}/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def tearDown(self):
+        self.client.close()
 
 
 class FileTest(TestCase):
     """Tests for file-related APIs."""
+
+    def setUp(self):
+        self.client = Client(har_file_path='akita_trace.file.har')
 
     def test_file_crud(self):
         chriss = {
@@ -66,7 +73,7 @@ Years Pro:      4""",
         }
 
         # Create file
-        response = client.post('/files/', chriss)
+        response = self.client.post('/files/', chriss, content_type="application/json")
         expected = copy.deepcopy(chriss)
         expected["id"] = response.data["id"]
         expected["created_at"] = response.data["created_at"]
@@ -75,21 +82,24 @@ Years Pro:      4""",
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Get file
-        response = client.get(f'/files/{expected["id"]}/')
+        response = self.client.get(f'/files/{expected["id"]}/')
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Update file 
         expected["contents"] = "Whoops deleted lol."
-        response = client.put(f'/files/{expected["id"]}/', data=expected, content_type='application/json')
+        response = self.client.put(f'/files/{expected["id"]}/', data=expected, content_type='application/json')
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Delete file
-        response = client.delete(f'/files/{expected["id"]}/')
+        response = self.client.delete(f'/files/{expected["id"]}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         # Try to get file, should fail
-        response = client.get(f'/files/{expected["id"]}/')
+        response = self.client.get(f'/files/{expected["id"]}/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def tearDown(self):
+        self.client.close()
 
